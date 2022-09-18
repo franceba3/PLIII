ffcc1e65187d711e58c9641f6ace45ab077c8aea
Erwin <57144439+n0rbb@users.noreply.github.com>
2022-09-18 18:31:17 +0200

    Plot-corrections-brewster-angle

--- LO6/LP/nlfit.R
+++ LO6/LP/nlfit.R
@@ -32,7 +32,7 @@ Fit <- gsl_nls(
 summary(Fit)
 
 
-## Park sex
+##Eval
 u <- seq(from = 15, to = 85, by = 0.2)
 eval <- function(x, n) {
   y = pi*x/180
@@ -43,12 +43,7 @@ ru <- eval(u, 1.49935)
 
 par(mfrow = c(1, 1), mar = c(4, 6, 2, 2))
 
-plot(data$t, data$r, xlab = expression(theta~(º)), ylab = expression(frac(R[perp]^2, A^2)), cex = 0.75, pch = 16)
-lines(u, ru, col="green")
-
-#Errorbars
-arrows(x0 = data$t - data$dt, y0 = data$r, x1 = data$t + data$dt, y1 = data$r, code = 3, angle = 90, length = 0.03, col = "black", lwd = 2)
-arrows(x0 = data$t, y0 = data$r - data$dr, x1 = data$t, y1 = data$r + data$dr, code = 3, angle = 90, length = 0.03, col = "black", lwd = 2)
+plot(data$t, data$r, xlab = expression(theta~(º)), ylab = expression(frac(R[perp]^2, A[perp]^2)), cex = 0.75, pch = 16)
 
 #Grid
 axis(1,
@@ -61,14 +56,20 @@ axis(2,
 
 box()
 
+lines(u, ru, col="green")
+
+#Errorbars
+arrows(x0 = data$t - data$dt, y0 = data$r, x1 = data$t + data$dt, y1 = data$r, code = 3, angle = 90, length = 0.03, col = "black", lwd = 2)
+arrows(x0 = data$t, y0 = data$r - data$dr, x1 = data$t, y1 = data$r + data$dr, code = 3, angle = 90, length = 0.03, col = "black", lwd = 2)
+
 legend(
   x = "topleft", inset = .02,
-  legend = c("Reflectivity"),
-  pch = 16,
-  col = c("Black"),
-  #legend = c("Reflectivity", "Regression"),
-  #pch = c(16, NA),
-  #lty = c(NA, 1),
-  #col = c("Black", "Green")
+  #legend = c("Reflectivity"),
+  #pch = 16,
+  #col = c("Black"),
+  legend = c("Reflectivity", "Regression"),
+  pch = c(16, NA),
+  lty = c(NA, 1),
+  col = c("Black", "Green")
 )
 
